764024fb337710e8df6d50921060c8942e67bffe
sourencho <souren@papazian.info>
2023-04-30 01:21:55 -0400

    Add tilemaps and shaders

--- Scripts/AnimatedBlockGroup.gd
+++ Scripts/AnimatedBlockGroup.gd
@@ -7,19 +7,19 @@ const animated_block_scene = preload("res://Scenes/AnimatedBlock.tscn")
 var animation_name: String
 
 const block_textures = [
-	preload("res://Assets/icon.png")
+    preload("res://Assets/icon.png")
 ]
 
 func _ready():
-	$AnimationPlayer.play(animation_name)
+    $AnimationPlayer.play(animation_name)
 
 func initialize(_animation_name: String):
-	animation_name = _animation_name
-	
+    animation_name = _animation_name
+    
 func add_block(grid_coord: Vector2, texture_idx: int):
-	var block = animated_block_scene.instance()
-	block.initialize(block_textures[texture_idx], grid_coord)
-	$Blocks.add_child(block)
+    var block = animated_block_scene.instance()
+    block.initialize(block_textures[texture_idx], grid_coord)
+    $Blocks.add_child(block)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	queue_free()
+    queue_free()
--- Scripts/DynamicTileMap.gd
+++ Scripts/DynamicTileMap.gd
@@ -1,26 +1,28 @@
 extends TileMap
 
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 func add_tile(pos: Vector2, sprite_idx: int):
-	set_cellv(pos, sprite_idx)
+    set_cellv(pos, sprite_idx)
 
 func clear_tile(pos: Vector2):
-	set_cellv(pos, -1)
+    set_cellv(pos, -1)
 
 func add_blocks(blocks: Dictionary):
-	for block in blocks:
-		add_block(block)
+    for block in blocks:
+        add_block(block)
 
 func clear_blocks(blocks: Dictionary):
-	for block in blocks:
-		clear_block(block)
+    for block in blocks:
+        clear_block(block)
 
 func add_block(block: Dictionary):
-	for cell in block["cells"]:
-		set_cellv(cell, 0)
+    for cell in block["cells"]:
+        set_cellv(cell, 1) 
+        update_bitmask_area(cell)
+
 
 func clear_block(block: Dictionary):
-	for cell in block["cells"]:
-		set_cellv(cell, -1)
+    for cell in block["cells"]:
+        set_cellv(cell, -1)
